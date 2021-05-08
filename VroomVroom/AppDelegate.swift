a7e8e4fbdfaf5a56ba0de849dcc9b3e8515c446e
Vivian Ross <vivianross06@users.noreply.github.com>
2021-05-08 11:37:26 -0700

    added Parse server keys

--- VroomVroom/AppDelegate.swift
+++ VroomVroom/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "uTalXsrLaPyQaT0C4ZlSl0iIKceHb2oXirnSOCMV"
+                    $0.clientKey = "zANdRYDcqhUlYm3WiqK6dmr2TwODBjjyp6zMs3LQ" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
