a0cf7ea24c09b14cf216a3c38094b35eecb63b15
Vivian Ross <vivianross06@users.noreply.github.com>
2021-05-15 11:11:45 -0700

    added log out functionality

---
+++ VroomVroom/FeedViewController.swift
@@ -0,0 +1,42 @@
+//
+//  FeedViewController.swift
+//  VroomVroom
+//
+//  Created by Vivian Ross on 5/15/21.
+//
+
+import UIKit
+import Parse
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else {return}
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
+    @IBAction func onPostButton(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
